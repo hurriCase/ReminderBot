20fd3f07a4dbce6475b5a8b47b863e691e1fbe86
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-02-13 20:13:00 +0300

    [fix] correct message

--- reminders.js
+++ reminders.js
@@ -10,7 +10,7 @@ export const reminders = pr
 	? [
 		{
 			cron: '0 17 * * *',
-			message: 'Напоминание сделать чек-ин в E-Book',
+			message: 'Напоминание сделать чек-ин в TBook - https://t.me/tbook\\_incentive\\_bot/tbook?startapp=WyIxMSIsIlZHWjJGTSJd',
 			chatIds: [pr],
 		}
 	]
